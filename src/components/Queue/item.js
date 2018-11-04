b3a7c2e22c2141229bfdddc6a98d3f51ee3ff30e
Akın Özgen <akinozgen17@outlook.com>
2018-11-04 23:50:48 +0300

    remove from queue

--- src/components/Queue/item.js
+++ src/components/Queue/item.js
@@ -1,13 +1,22 @@
 import React, { useContext } from 'react';
 import Store from '../../context';
 
-import { getTitle } from '../Meta';
-
 export default function Item(item) {
-  const { state } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
 
   const { queue_position } = state;
   const current = item.index === queue_position;
+
+  function remove() {
+    const newQueue = state.queue.filter((_, i) => i !== item.index);
+    dispatch({ type: 'updateQueue', payload: newQueue })
+
+    if (newQueue.length === 0) {
+      dispatch({ type: 'updateCurrentSong', payload: null });
+      dispatch({ type: 'updateIsPlaying', payload: 0 });
+      dispatch({ type: 'updateQueuePosition', payload: 0 });
+    }
+  }
 
   return <li className={`list-group-item ${current ? 'active' : ''}`}>
     <span>
@@ -19,7 +28,7 @@ export default function Item(item) {
       {item.snippet.title}
     </span>
     <span>
-      <button className="btn btn-transparent btn-sm">
+      <button className="btn btn-transparent btn-sm" onClick={remove}>
         <i className="fa fa-times"></i>
       </button>
     </span>
