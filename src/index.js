2698a8bfb05b773f39cbc43e5dd05cc3633f0b20
Akın Özgen <akinozgen17@outlook.com>
2018-11-05 00:13:01 +0300

    todo

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
+
+// TODO: List group text wraps to be fixed
+// TODO: Forward & Backward error when queue empty to be fixed
+// TODO: Get mp3 url of current song when play triggered
+// TODO: Play audio with <audio> to be implemented
+// TODO: <audio> onprogress -> Progressbar,
+//               play button -> play, pause,
+//               forward, backward -> change src
+//               onfinish -> forward & (on last queue item -> goto first)
+//               is_repeat(1) -> !forward
