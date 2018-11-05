78c196b14a328a81e2584be54334066296faeafa
Akın Özgen <akinozgen17@outlook.com>
2018-11-05 22:50:30 +0300

    progressbar working

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Store from '../../context';
 
 export function getTitle(item) {
@@ -11,7 +11,7 @@ export default function Meta() {
   return <div className="meta">
     <div className="seekbar">
       <div className="progress">
-        <div className="progress-bar" role="progressbar" style={{ width: '20%' }} aria-valuenow="20" aria-valuemin="0"
+        <div className="progress-bar" role="progressbar" style={{ width: `${state.progress}%` }} aria-valuenow={state.progress} aria-valuemin="0"
           aria-valuemax="100"></div>
       </div>
     </div>
