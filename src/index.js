7869ac5bd67a36b5a374ba7ffd9d26a4ceb26974
Akın Özgen <akinozgen17@outlook.com>
2018-11-06 13:46:23 +0300

    todos removed. all todos and fix reminders are in issues now!

--- src/index.js
+++ src/index.js
@@ -6,8 +6,3 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
-
-// TODO: List group text wraps to be fixed
-// TODO: <audio> is_repeat(1) -> !forward
-// TODO: Move functions outside the components and export them
-// TODO: Sort queue
