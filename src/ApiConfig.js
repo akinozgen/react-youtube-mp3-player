8ad1f059822d11b0cf0d02950c510e1d782a2a0c
Akın Özgen <akin.ozgen17@gmail.com>
2019-11-09 08:33:57 +0000

    Update ApiConfig.js

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -2,7 +2,7 @@ const API_CONFIG = {
   yt_api_key: 'AIzaSyD7GI67U8jOax0uBV301aF9XnJIA9XJfKE',
   yt_search_endpoint: _ => `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${_.searchQuery}&key=${API_CONFIG.yt_api_key}`,
   yt_video_info_endpoint: _ => `https://www.googleapis.com/youtube/v3/videos?id=${_.videoId}&part=contentDetails&key=${API_CONFIG.yt_api_key}`,
-  yt_mp3_endpoint: _ => `http://159.65.167.192:8080/get-mp3/${_.videoId}`,
+  yt_mp3_endpoint: _ => `http://serene-caverns-76533.herokuapp.com/get-mp3/${_.videoId}`,
 };
 
 export default API_CONFIG;
