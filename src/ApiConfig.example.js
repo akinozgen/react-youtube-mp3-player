ee4bdd17bd70e1016ee3e5321b14a2b08ec95d75
Akin Ozgen <akinozgen17@outlook.com>
2021-10-20 16:18:12 +0300

    config example file created

--- src/ApiConfig.example.js
+++ src/ApiConfig.example.js
@@ -1,8 +1,8 @@
 const API_CONFIG = {
-  yt_api_key: 'AIzaSyD7GI67U8jOax0uBV301aF9XnJIA9XJfKE',
+  yt_api_key: 'YOUR_API_KEY',
   yt_search_endpoint: _ => `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${_.searchQuery}&key=${API_CONFIG.yt_api_key}`,
   yt_video_info_endpoint: _ => `https://www.googleapis.com/youtube/v3/videos?id=${_.videoId}&part=contentDetails&key=${API_CONFIG.yt_api_key}`,
-  yt_mp3_endpoint: _ => `http://serene-caverns-76533.herokuapp.com/get-mp3/${_.videoId}`,
+  yt_mp3_endpoint: _ => `YOUR_API_ENDPOINT/${_.videoId}`,
 };
 
 export default API_CONFIG;
